705462ced94c7df02dee15b7991da2bbc0b33ebd
Gabriella Lanouette <30821084+gablanouette@users.noreply.github.com>
2020-08-18 23:55:35 -0400

    Create uninformed_search.py

---
+++ uninformed_search.py
@@ -0,0 +1,25 @@
+# This project is a wonderful introduction project when learning Artificial Intelligence
+# This project will visualize graphs 
+
+# This purpose of this project is to:
+#   - use Python to represent trees and graphs
+#   - manipulate trees and graphs
+#   - visualize trees and graphs
+#   - use Python for uninformed graph search
+
+# a graph is a collection of vertices, or nodes, and edges, which are connections between different 
+# vertices (https://en.wikipedia.org/wiki/Graph_(discrete_mathematics). These graphs are visually
+# represented with labels enclosed in a circle for the vertices, and lines connecting vertices
+# which are the edges.
+
+# a directed graph has edges that are arrows, meaning there is a connection from one vertex to 
+# another, any other graph is an undirected graph.
+
+# Python library to use when working with graphs: networkx
+
+# import packages
+import pandas as pd
+import networkx as nx
+import warnings
+warnings.filterwarnings('ignore')
+
